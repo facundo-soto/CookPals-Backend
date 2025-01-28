3a12c07d17942ab266e02d636a47d79fd7380c01
FacundoSoto <facundosotops@gmail.com>
2025-01-27 23:12:42 -0300

    Más ajustes
    Más ajustes

--- src/config/config.js
+++ src/config/config.js
@@ -4,5 +4,6 @@ dotenv.config();
 export const {
     PORT = 0,
     IMGUR_CLIENT_ID = "",
-    FIREBASE_CONFIG = "{}"
+    FIREBASE_CONFIG = "{}",
+    FRONTEND_URL = ""
 } = process.env;
--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import recipesRoutes from './routes/recipesRoutes.js';
 import profileRoutes from './routes/profileRoutes.js';
-import { PORT } from './config/config.js';
+import { PORT, FRONTEND_URL } from './config/config.js';
 
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000', // Reemplaza con el dominio de tu frontend
+    origin: FRONTEND_URL, // Reemplaza con el dominio de tu frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
     credentials: true // Si necesitas enviar cookies o encabezados personalizados
 }));
